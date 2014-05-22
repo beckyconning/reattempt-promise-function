d393059f349e159c3422257e617449d72eabd835
Dora Coder <dora@palehor.se>
2014-05-22 17:40:07 +0200

    added async control over mock requestPromise

--- spec/couch-init-spec.js
+++ spec/couch-init-spec.js
@@ -4,98 +4,143 @@
 // thanks! : )
 
 describe('couch-init', function() {
-  var couchInit;
-  var requestPromise;
-  var Q = require('q');
-  var proxyquire = require('proxyquire');
-  var couchUrl = "http://example.com";
-  var couchInitScope = {};
+    var couchInit;
+    var requestPromise;
+    var requestDeferreds;
+    var flushRequests;
+    var RSVP = require('rsvp');
+    var proxyquire = require('proxyquire');
+    var couchUrl = "http://example.com";
 
-  // create and spy on request-promise mock
-  beforeEach(function() {
-    couchInitScope.requestPromise = function() {
-      // return a resolved promise
-      return Q('"previous value"');
-    }; 
-    spyOn(couchInitScope, 'requestPromise').andCallThrough();
-    requestPromise = couchInitScope.requestPromise;
+    // create and spy on request-promise mock
+    beforeEach(function() {
+        var mock = {};
+        requestDeferreds = [];
 
-    // instantiate a couch-init with overridden dependencies
-    couchInit = proxyquire('../lib/couch-init', {
-      'request-promise': requestPromise 
-    });
-  });
+        // mock requestPromise
+        mock.requestPromise = function() {
+            requestDeferred = RSVP.defer();
+            requestDeferreds.push(requestDeferred);
+            return requestDeferred.promise;
+        }; 
 
-  describe('configure', function() {
-    it('should return a promise', function() {
-      var configuration = {};
-      var promise = couchInit.configure(couchUrl, configuration);
-      expect(promise.then).toBeDefined();
-    });
+        // spy on mock requestPromise
+        spyOn(mock, 'requestPromise').andCallThrough();
+        requestPromise = mock.requestPromise;
 
-    it('should update the couchdb configuration for each option in the provided configuration', function(done) {
-      var configuration = {
-        httpd: {
-          enable_cors: 'true'
-        },  
-        cors: {
-          origins: '*',
-          credentials: 'false'
-        }   
-      };    
-      var expectedConfigOptions = {
-        "http://example.com/_config/httpd/enable_cors": "true",
-        "http://example.com/_config/cors/origins": "*",
-        "http://example.com/_config/cors/credentials": "false"
-      };
+        // helper method to complete mock requests 
+        flushRequests = function() {
+            requestDeferreds.forEach(function(deferred) {
+                deferred.resolve('Couch response');
+            });
+        };
 
-      var expectRequestsToHaveBeenMadeCorrectlyForEachOption = function() {
-        Object.keys(expectedConfigOptions).forEach(function(optionUrl) {
-          var optionValue = expectedConfigOptions[optionUrl];
-          var expectedRequestOptions = {
-            url: optionUrl,
-            method: 'PUT',
-            json: true,
-            body: JSON.stringify(optionValue)
-          };
-          expect(requestPromise).toHaveBeenCalledWith(expectedRequestOptions);
+        // instantiate a couch-init with overridden dependencies
+        couchInit = proxyquire('../lib/couch-init', {
+            'request-promise': requestPromise 
         });
-      };
-
-      // call the method
-      couchInit.configure(couchUrl, configuration)
-        .then(expectRequestsToHaveBeenMadeCorrectlyForEachOption)
-        .then(done)
-        .catch(console.error.bind(console));
     });
-  });
 
-  describe('createDatabases', function() {
-    it('should return a promise', function() {
-      var configuration = {};
-      var promise = couchInit.createDatabases(couchUrl, configuration);
-      expect(promise.then).toBeDefined();
-    });  
-
-    it('should create a couchdb database for each database name in the provided array', function(done) {
-      var databaseNames = ['database1', 'database2', 'database3'];
-
-      var expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName = function() {
-        databaseNames.forEach(function(databaseName) {
-          var expectedRequestOptions = {
-            url: couchUrl + '/' + databaseName,
-            method: 'PUT',
-          };
-          expect(requestPromise).toHaveBeenCalledWith(expectedRequestOptions);
+    describe('configure', function() {
+        it('should return a promise', function() {
+            var configuration = {};
+            var promise = couchInit.configure(couchUrl, configuration);
+            expect(promise.then).toBeDefined();
         });
-      };
 
-      // call the method
-      couchInit.createDatabases(couchUrl, databaseNames)
-        .then(expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName)
-        .then(done)
-        .catch(console.error.bind(console));
+        it('should update the couchdb configuration for each option in the provided configuration', function(done) {
+            var configuration = {
+                httpd: {
+                    enable_cors: 'true'
+                },    
+                cors: {
+                    origins: '*',
+                    credentials: 'false'
+                }     
+            };        
+            var expectedConfigOptions = {
+                "http://example.com/_config/httpd/enable_cors": "true",
+                "http://example.com/_config/cors/origins": "*",
+                "http://example.com/_config/cors/credentials": "false"
+            };
+
+            var expectRequestsToHaveBeenMadeCorrectlyForEachOption = function() {
+                Object.keys(expectedConfigOptions).forEach(function(optionUrl) {
+                    var optionValue = expectedConfigOptions[optionUrl];
+                    var expectedRequestOptions = {
+                        url: optionUrl,
+                        method: 'PUT',
+                        json: true,
+                        body: JSON.stringify(optionValue)
+                    };
+                    expect(requestPromise).toHaveBeenCalledWith(expectedRequestOptions);
+                });
+            };
+
+            // call the method
+            couchInit.configure(couchUrl, configuration)
+                .then(expectRequestsToHaveBeenMadeCorrectlyForEachOption)
+                .then(done)
+                .catch(console.error.bind(console));         
+
+            flushRequests();
+        }); 
     });
-  });
+
+    //describe('waitForCouch', function() {
+    //    it('should return a promise', funciton() {
+    //        var promise = couchInit.waitForCouch(couchUrl);
+    //        expect(promise.then).toBeDefined();
+    //    });    
+
+    //    it('should not resolve the promise until couch is available', function(done) {
+    //        var expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName = function() {
+    //            databaseNames.forEach(function(databaseName) {
+    //                var expectedRequestOptions = {
+    //                    url: couchUrl,
+    //                    method: 'GET',
+    //                };
+    //                expect(requestPromise).toHaveBeenCalledWith(expectedRequestOptions);
+    //            });
+    //        };
+
+    //        // call the method
+    //        couchInit.createDatabases(couchUrl, databaseNames)
+    //            .then(expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName)
+    //            .then(done)
+    //            .catch(console.error.bind(console));
+    //    });
+    //});
+
+    describe('createDatabases', function() {
+        it('should return a promise', function() {
+            var configuration = {};
+            var promise = couchInit.createDatabases(couchUrl, configuration);
+            expect(promise.then).toBeDefined();
+        });    
+
+        it('should create a couchdb database for each database name in the provided array', function(done) {
+            var databaseNames = ['database1', 'database2', 'database3'];
+
+            var expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName = function() {
+                databaseNames.forEach(function(databaseName) {
+                    var expectedRequestOptions = {
+                        url: couchUrl + '/' + databaseName,
+                        method: 'PUT',
+                    };
+                    expect(requestPromise).toHaveBeenCalledWith(expectedRequestOptions);
+                });
+            };
+
+            // call the method
+            couchInit.createDatabases(couchUrl, databaseNames)
+                .then(expectRequestsToHaveBeenMadeCorrectlyForEachDatabaseName)
+                .then(done)
+                .catch(console.error.bind(console));
+
+            // flush the request
+            flushRequests();
+        });
+    });
 
 });
