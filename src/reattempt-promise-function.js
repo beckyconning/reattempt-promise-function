2605621d8743b04e15a154e680b735e06768f573
Becky Conning <becky.conning@gmail.com>
2014-11-10 02:03:10 +0000

    minor refactor

--- src/reattempt-promise-function.js
+++ src/reattempt-promise-function.js
@@ -5,15 +5,14 @@ var PromiseType = T.subtype(T.Obj, function (o) { return o instanceof Promise; }
 
 var reattempt = T.func([ T.Func, T.Arr, T.Num, T.Num ], PromiseType)
     .of(function (promiseFunction, args, delay, attemptsLeft) {
+        var nextAttempt = function () {
+            return reattempt(promiseFunction, args, delay, attemptsLeft - 1);
+        };
+
         return Promise.resolve(promiseFunction.apply(promiseFunction, args))
             .catch(function (rejectValue) {
-                if (attemptsLeft <= 1)
-                    return Promise.reject(rejectValue);
-                else
-                    return Promise.delay(delay)
-                        .then(function () {
-                                return reattempt(promiseFunction, args, delay, attemptsLeft - 1);
-                        });
+                if (attemptsLeft <= 1) return Promise.reject(rejectValue);
+                else return Promise.delay(delay).then(nextAttempt);
             });
     });
 
